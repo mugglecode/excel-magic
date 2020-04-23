f04e5588b2def2d0a93729c9494b59530a1e3c61
Kelly <guojiyou40020@hotmail.com>
2020-04-23 17:57:32 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='excel-magic',
+        version='1.0.0',
+        packages=[''],
+        url='https://github.com/guo40020/excel-magic',
+        license='MIT',
+        author='Kelly',
+        author_email='',
+        description=''
+)
