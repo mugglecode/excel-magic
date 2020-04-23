0d0e0c0c8c3fcdab69783cc895bf8da2c9aa9fc8
Kelly <guojiyou40020@hotmail.com>
2020-04-23 18:15:12 +0800

    update

--- excelMagic/setup.py
+++ excelMagic/setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(
-        name='excelMagic',
+        name='excel-magic',
         version='1.0.0',
-        packages=[''],
-        url='https://github.com/guo40020/excel-magic',
+        packages=['excelMagic'],
+        package_dir={'': 'excelMagic'},
+        url='github.com/guo40020/excel-magic',
         license='MIT',
         author='Kelly',
         author_email='',
