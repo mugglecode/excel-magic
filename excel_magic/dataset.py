e450cfeb258362e8bd116295df2e1ef5e604da3c
Kelly <guojiyou40020@hotmail.com>
2020-04-26 14:57:50 +0800

    bug fix

--- excel_magic/dataset.py
+++ excel_magic/dataset.py
@@ -265,7 +265,7 @@ class Dataset:
             sheet = workbook.add_worksheet(table.name)
             pointer = Pointer(0, 0)
             for field in table.fields:
-                sheet.write(pointer.row, pointer.col, field, table.header_style)
+                sheet.write(pointer.row, pointer.col, field, workbook.add_format(table.header_style))
                 pointer.next_col()
             pointer.next_row()
             for data_row in table.data_rows:
