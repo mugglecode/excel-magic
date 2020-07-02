827796e95f7ce78b73105e4fa5930a139aa09632
Kelly <guojiyou40020@hotmail.com>
2020-07-02 17:35:18 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 from setuptools import setup
 
 setup(
-        name='excelmagic',
+        name='excelmagic2',
         version='2.0.0',
         packages=setuptools.find_packages(),
-        url='https://github.com/guo40020/excel-magic',
+        url='https://github.com/mugglecode/excel-magic/tree/2.0',
         license='MIT',
         author='Kelly',
         author_email='',
         description='',
-        install_requires=['xlrd', 'XlsxWriter', 'Pillow'],
-        python_requires='>3.7'
+        install_requires=['openpyxl', 'XlsxWriter', 'Pillow'],
+        python_requires='>3.7.2'
 )
